cfcb3991160eaccabd84d05f34a812ac8e87326d
Alejandro Medrano <u201924161@upc.edu.pe>
2021-09-30 00:51:33 -0500

    feat: connection with json-server in home
    I connected the data of the json-server with the home page of the application (section offers)

--- src/agency/pages/hired-services.vue
+++ src/agency/pages/hired-services.vue
@@ -3,13 +3,26 @@
     <v-col cols="12">
       <v-card class="mx-auto">
         <v-data-table :headers="headers" :items="clients" sort-by="name" class="elevation-1" show-group-by>
-          <template v-slot:top>
-            <v-toolbar flat>
-              <!-- Change status -->
-              <v-dialog>
 
-              </v-dialog>
-            </v-toolbar>
+          <template v-slot:item.status="props">
+            <v-edit-dialog
+                large
+                persistent
+            >
+              <div>{{ props.item.status }}</div>
+
+              <template v-slot:input>
+                <div class="mt-4 text-h6">
+                  Update status
+                </div>
+                <v-select
+                    :items = "serviceStatus"
+                    v-model="props.item.status"
+                    label="Edit"
+                ></v-select>
+              </template>
+
+            </v-edit-dialog>
           </template>
         </v-data-table>
       </v-card>
@@ -25,20 +38,29 @@ export default {
   name: "hired-services",
   data: () => ({
     headers: [
-      { text: 'Nombre y apellidos', align: 'start', value: 'name', groupable: false  },
-      { text: 'Correo', align: 'left', value: 'email', sortable: false, groupable: false },
-      { text: 'Teléfono', align: 'left', value: 'phoneNumber', sortable: false, groupable: false },
-      { text: 'Cantidad (personas)', align: 'left', value: 'amount', groupable: false },
-      { text: 'Servicio', align: 'left', value: 'service'},
-      { text: 'Fecha de salida', align: 'left', value: 'scheduledDate', groupable: false },
-      { text: 'Monto', align: 'left', value: 'price', groupable: false  },
-      { text: 'Estado', align: 'left', value: 'status', groupable: false },
+      {text: 'Nombre y apellidos', align: 'start', value: 'name', groupable: false},
+      {text: 'Correo', align: 'left', value: 'email', sortable: false, groupable: false},
+      {text: 'Teléfono', align: 'left', value: 'phoneNumber', sortable: false, groupable: false},
+      {text: 'Cantidad (personas)', align: 'left', value: 'amount', groupable: false},
+      {text: 'Servicio', align: 'left', value: 'service'},
+      {text: 'Fecha de salida', align: 'left', value: 'scheduledDate', groupable: false},
+      {text: 'Monto', align: 'left', value: 'price', groupable: false},
+      {text: 'Estado', align: 'left', value: 'status', groupable: false},
     ],
     clients: [],
     dialogChangeStatus: false,
     dialogDelete: false,
     editIndex: -1,
-    editedItem: {name: '', email: '', phoneNumber: '', amount: '', service: '', scheduledDate: '', price: 0, status: ''},
+    editedItem: {
+      name: '',
+      email: '',
+      phoneNumber: '',
+      amount: '',
+      service: '',
+      scheduledDate: '',
+      price: 0,
+      status: ''
+    },
     serviceStatus: ['pending', 'active', 'finished'],
   }),
 
@@ -78,7 +100,7 @@ export default {
 
           HiredService.getExtendInformation("service")
               .then((response) => {
-                for(let position = 0; position < response.data.length; position++) {
+                for (let position = 0; position < response.data.length; position++) {
                   this.clients[position].service = response.data[position].service.name;
                 }
                 console.log(response.data);
@@ -99,10 +121,10 @@ export default {
 
     deleteHiredService(id) {
       HiredService.delete(id)
-          .then( () => {
+          .then(() => {
             this.refreshList();
           })
-          .catch( e => {
+          .catch(e => {
             console.log(e);
           });
     },
@@ -110,7 +132,7 @@ export default {
     changeStatusItem(item) {
       this.editedIndex = this.clients.indexOf(item);
       this.editedItem = Object.assign({}, item);
-      this.dialogDelete = true;
+      this.dialogChangeStatus = true;
     },
 
     deleteItem(item) {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,9 @@
 <template>
   <v-main app class="secondary">
-    <v-container >
+    <v-container>
       <v-row class="d-flex flex-md-row flex-xl-column">
         <v-col cols="12" class="col-md-3">
+          <!--Filters-->
           <v-card class="rounded-lg pa-1">
             <v-list>
               <v-subheader class="font-weight-bold title">Categorias</v-subheader>
@@ -20,225 +21,68 @@
             </v-list>
           </v-card>
         </v-col>
+
         <v-col cols="12" class="col-md-9">
           <!-- SECTION: OFFERS -->
           <v-card class="py-4 px-8 mb-4 rounded-lg">
             <v-list>
               <v-subheader class="title font-weight-bold pl-0">Ofertas del día</v-subheader>
-              <v-list-item-group >
+              <v-list-item-group>
                 <v-row>
-                  <v-col cols="12" class="col-md-3">
-                    <v-card
-                        :loading="loading"
-                        class="my-2"
-                        max-width="374"
-                    >
-                      <template slot="progress">
-                        <v-progress-linear
-                            color="deep-purple"
-                            height="10"
-                            indeterminate
-                        ></v-progress-linear>
-                      </template>
+                    <v-col cols="12" class="col-md-3" v-for="service in services" :key="service.id">
+                      <v-card
+                          :loading="loading"
+                          class="my-2"
+                          max-width="374"
+                      >
+                        <template slot="progress">
+                          <v-progress-linear
+                              color="deep-purple"
+                              height="100"
+                              indeterminate
+                          ></v-progress-linear>
+                        </template>
 
-                      <a href="">
-                        <v-img
-                            height=120
-                            src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
-                        ></v-img>
-                      </a>
+                        <a href="">
+                          <v-img
+                              height=120
+                              :src="service.photos"
+                          ></v-img>
+                        </a>
 
-                      <a href="" class="text-decoration-none">
-                        <v-card-title class="d-flex flex-column align-center pb-0 black--text">Cafe Badilico</v-card-title>
-                      </a>
+                        <a href="" class="text-decoration-none">
+                          <v-card-title class="d-flex flex-column align-center pb-0 black--text text-sm-body-2"> {{ service.name }} </v-card-title>
+                        </a>
 
-                      <v-card-text class="d-flex flex-column align-center">
-                        <v-row
-                            align="center"
-                            class="mx-0"
-                        >
-                          <a href="" class="text-decoration-none">
-                            <v-rating
-                                :value="4"
-                                color="amber"
-                                dense
-                                half-increments
-                                readonly
-                                size="20"
-                            ></v-rating>
-                          </a>
-                        </v-row>
+                        <v-card-text class="d-flex flex-column align-center">
+                          <v-row
+                              align="center"
+                              class="mx-0"
+                          >
+                            <a href="" class="text-decoration-none">
+                              <v-rating
+                                  :value="service.score"
+                                  color="amber"
+                                  dense
+                                  half-increments
+                                  readonly
+                                  size="20"
+                              ></v-rating>
+                            </a>
+                          </v-row>
 
-                        <div class="pt-3 text-subtitle-1 d-flex flex-row">
-                          <a href="" class="text-decoration-none ">
-                            <div class="text-decoration-line-through pr-2 black--text">$500</div>
-                          </a>
-                          <a href="" class="text-decoration-none">
-                            <div class="black--text">$480</div>
-                          </a>
-                        </div>
-                      </v-card-text>
-                    </v-card>
-                  </v-col>
-                  <v-col cols="12" class="col-md-3">
-                    <v-card
-                        :loading="loading"
-                        class="my-2"
-                        max-width="374"
-                    >
-                      <template slot="progress">
-                        <v-progress-linear
-                            color="deep-purple"
-                            height="10"
-                            indeterminate
-                        ></v-progress-linear>
-                      </template>
+                          <div class="pt-3 text-subtitle-1 d-flex flex-row">
+                            <a href="" class="text-decoration-none ">
+                              <div class="text-decoration-line-through pr-2 black--text">$500</div>
+                            </a>
+                            <a href="" class="text-decoration-none">
+                              <div class="black--text"> {{ `$${service.price}.00` }} </div>
+                            </a>
+                          </div>
+                        </v-card-text>
+                      </v-card>
+                    </v-col>
 
-                      <a href="">
-                        <v-img
-                            height=120
-                            src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
-                        ></v-img>
-                      </a>
-
-                      <a href="" class="text-decoration-none">
-                        <v-card-title class="d-flex flex-column align-center pb-0 black--text">Cafe Badilico</v-card-title>
-                      </a>
-
-                      <v-card-text class="d-flex flex-column align-center">
-                        <v-row
-                            align="center"
-                            class="mx-0"
-                        >
-                          <a href="" class="text-decoration-none">
-                            <v-rating
-                                :value="4"
-                                color="amber"
-                                dense
-                                half-increments
-                                readonly
-                                size="20"
-                            ></v-rating>
-                          </a>
-                        </v-row>
-
-                        <div class="pt-3 text-subtitle-1 d-flex flex-row">
-                          <a href="" class="text-decoration-none ">
-                            <div class="text-decoration-line-through pr-2 black--text">$500</div>
-                          </a>
-                          <a href="" class="text-decoration-none">
-                            <div class="black--text">$480</div>
-                          </a>
-                        </div>
-                      </v-card-text>
-                    </v-card>
-                  </v-col>
-                  <v-col cols="12" class="col-md-3">
-                    <v-card
-                        :loading="loading"
-                        class="my-2"
-                        max-width="374"
-                    >
-                      <template slot="progress">
-                        <v-progress-linear
-                            color="deep-purple"
-                            height="10"
-                            indeterminate
-                        ></v-progress-linear>
-                      </template>
-
-                      <a href="">
-                        <v-img
-                            height=120
-                            src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
-                        ></v-img>
-                      </a>
-
-                      <a href="" class="text-decoration-none">
-                        <v-card-title class="d-flex flex-column align-center pb-0 black--text">Cafe Badilico</v-card-title>
-                      </a>
-
-                      <v-card-text class="d-flex flex-column align-center">
-                        <v-row
-                            align="center"
-                            class="mx-0"
-                        >
-                          <a href="" class="text-decoration-none">
-                            <v-rating
-                                :value="4"
-                                color="amber"
-                                dense
-                                half-increments
-                                readonly
-                                size="20"
-                            ></v-rating>
-                          </a>
-                        </v-row>
-
-                        <div class="pt-3 text-subtitle-1 d-flex flex-row">
-                          <a href="" class="text-decoration-none ">
-                            <div class="text-decoration-line-through pr-2 black--text">$500</div>
-                          </a>
-                          <a href="" class="text-decoration-none">
-                            <div class="black--text">$480</div>
-                          </a>
-                        </div>
-                      </v-card-text>
-                    </v-card>
-                  </v-col>
-                  <v-col cols="12" class="col-md-3">
-                    <v-card
-                        :loading="loading"
-                        class="my-2"
-                        max-width="374"
-                    >
-                      <template slot="progress">
-                        <v-progress-linear
-                            color="deep-purple"
-                            height="10"
-                            indeterminate
-                        ></v-progress-linear>
-                      </template>
-
-                      <a href="">
-                        <v-img
-                            height=120
-                            src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
-                        ></v-img>
-                      </a>
-
-                      <a href="" class="text-decoration-none">
-                        <v-card-title class="d-flex flex-column align-center pb-0 black--text">Cafe Badilico</v-card-title>
-                      </a>
-
-                      <v-card-text class="d-flex flex-column align-center">
-                        <v-row
-                            align="center"
-                            class="mx-0"
-                        >
-                          <a href="" class="text-decoration-none">
-                            <v-rating
-                                :value="4"
-                                color="amber"
-                                dense
-                                half-increments
-                                readonly
-                                size="20"
-                            ></v-rating>
-                          </a>
-                        </v-row>
-
-                        <div class="pt-3 text-subtitle-1 d-flex flex-row">
-                          <a href="" class="text-decoration-none ">
-                            <div class="text-decoration-line-through pr-2 black--text">$500</div>
-                          </a>
-                          <a href="" class="text-decoration-none">
-                            <div class="black--text">$480</div>
-                          </a>
-                        </div>
-                      </v-card-text>
-                    </v-card>
-                  </v-col>
                 </v-row>
 
               </v-list-item-group>
@@ -675,8 +519,30 @@
 </template>
 
 <script>
+import ServicesService from '../agency/services/services.service'
+
 export default {
-  name: "Home"
+  name: "Home",
+  data: () => ({
+    services: []
+  }),
+
+  methods: {
+    retrieveServices(){
+      ServicesService.getAll()
+          .then((response) => {
+            this.services = response.data;
+            console.log(response.data);
+          })
+          .catch(e => {
+            console.log(e);
+          })
+    }
+  },
+
+  mounted() {
+    this.retrieveServices();
+  }
 }
 </script>
 
